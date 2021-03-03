8c0146f49999d5078fb8ae5414f6e0de1b4a82dc
Makar Pundis <makar.pundis@yandex.ru>
2021-03-04 01:16:22 +0300

    added lab1

---
+++ ChatApp/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ChatApp
+{
+    public class Person
+    {
+        public Person(string id, IPEndPoint localIp, IPEndPoint remoteIp)
+        {
+            Id = id;
+            LocalIp = localIp;
+            RemoteIp = remoteIp;
+        }
+
+        public string Id { get; init; }
+        public IPEndPoint LocalIp { get; init; }
+        public IPEndPoint RemoteIp { get; set; }
+    }
+}
